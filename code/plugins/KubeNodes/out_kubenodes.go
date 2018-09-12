b9606b1bd02600d6efe00debe311a1036557f85d
rashmy <rashmy@RASHMY-PC2>
2018-09-12 14:14:51 -0700

    kubenodes

--- code/plugins/KubeNodes/out_kubenodes.go
+++ code/plugins/KubeNodes/out_kubenodes.go
@@ -3,24 +3,22 @@ package main
 import "github.com/fluent/fluent-bit-go/output"
 import (
 	"fmt"
-	"unsafe"
 	"C"
 	"flag"
 	"log"
 	"encoding/json"
+	"unsafe"
 	"crypto/tls"
 	"net/http"
 	"bytes"
 	"time"
 	 metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	 "k8s.io/client-go/kubernetes"
-	 v1 "k8s.io/api/core/v1"
-	 "k8s.io/apimachinery/pkg/types"
 	 "strings"
 	 "os"
 	"github.com/mitchellh/mapstructure"
 	"k8s.io/client-go/rest"
-	"math"
+	"reflect"
 )
 
 var (
@@ -31,23 +29,22 @@ var (
 	certFile = flag.String("cert", "/etc/opt/microsoft/omsagent/6d3a50d0-808e-4d66-86c0-7b99d810ffe1/certs/oms.crt", "OMS Agent Certificate")
 )
 
-
 // DataItem represents the object corresponding to the json that is sent by fluentbit tail plugin
 type DataItem struct {
-	Timestamp        string                   `json:"Timestamp"`
-	Host             string                   `json:"Host"`
-	ObjectName       string                   `json:"ObjectName"`
-	InstanceName     string                   `json:"InstanceName"`
-	Collections      []MetricCollection       `json:"Collections"`
+	CollectionTime              string                   `json:"CollectionTime"`
+	Computer                    string                   `json:"Computer"`
+	ClusterName                 string                   `json:"ClusterName"`
+	ClusterId                   string                   `json:"ClusterId"`
+	CreationTimeStamp           string                   `json:"CreationTimeStamp"`
+	Labels                      string                   `json:"Labels"`
+	Status                      string                   `json:"Status"`
+	LastTransitionTimeReady     string                   `json:"LastTransitionTimeReady"`
+	KubeletVersion              string                   `json:"KubeletVersion"`
+	KubeProxyVersion            string                   `json:"KubeProxyVersion"`
 }
 
-type MetricCollection struct {
-	CounterName       string       `json:"CounterName"`
-	Value             float64      `json:"Value"`
-}
-
-// KubePerfBlob represents the object corresponding to the payload that is sent to the ODS end point
-type KubePerfBlob struct {
+// KubeNodeInventoryBlob represents the object corresponding to the payload that is sent to the ODS end point
+type KubeNodeInventoryBlob struct {
 	DataType  string     `json:"DataType"`
 	IPName    string     `json:"IPName"`
 	DataItems []DataItem `json:"DataItems"`
@@ -55,7 +52,7 @@ type KubePerfBlob struct {
 
 //export FLBPluginRegister
 func FLBPluginRegister(ctx unsafe.Pointer) int {
-	return output.FLBPluginRegister(ctx, "kubeperf", "Stdout GO!")
+	return output.FLBPluginRegister(ctx, "kubenodes", "Stdout GO!")
 }
 
 //export FLBPluginInit
@@ -71,7 +68,6 @@ func FLBPluginInit(ctx unsafe.Pointer) int {
 var ClusterId string
 var ClusterName string
 var clientset *kubernetes.Clientset
-var NodeMetrics map[string]float64
 
 func getClusterName() string {
 	if (ClusterName != "") {
@@ -106,7 +102,7 @@ func getClusterName() string {
 		}
 	}
 	return ClusterName
-} 
+}
 
 func getClusterId() string{
 	if ClusterId != "" {
@@ -122,136 +118,6 @@ func getClusterId() string{
     return ClusterId
 }
 
-func getContainerResourceRequestsAndLimits(pods *v1.PodList, metricCategory string, metricNameToCollect string, metricNametoReturn string) []DataItem{
-	var metricItems []DataItem
-	clusterId := getClusterId()
-	for _, pod := range pods.Items {
-		var metricItem DataItem
-		var metricCounterItems []MetricCollection
-		
-		podmetadata := pod.ObjectMeta
-		podNameSpace := podmetadata.Namespace
-		var podUid types.UID
-		if podNameSpace == "kube-system" && podmetadata.OwnerReferences == nil {
-            // The above case seems to be the only case where you have horizontal scaling of pods
-            // but no controller, in which case cAdvisor picks up kubernetes.io/config.hash
-            // instead of the actual poduid. Since this uid is not being surface into the UX
-            // its ok to use this.
-            // Use kubernetes.io/config.hash to be able to correlate with cadvisor data
-				podUid = types.UID(podmetadata.Annotations["kubernetes.io/config.hash"])
-				//['kubernetes.io/config.hash']
-		} else {
-			podUid = podmetadata.UID
-		}
-
-		if pod.Spec.Containers != nil && pod.Spec.NodeName != "" {
-			nodeName := pod.Spec.NodeName
-			record := make(map[interface{}]interface{})
-			for _, container := range pod.Spec.Containers {
-				containerName := container.Name
-				currentTime := time.Now()
-				metricTime := currentTime.UTC().Format(time.RFC3339)
-				var metricValue float64
-				//if container.Resources[metricCategory] != nil && container.Resources[metricCategory][metricNameToCollect] != nil {
-				switch metricNameToCollect {
-					case "cpu": if metricCategory == "limits" {
-									if container.Resources.Limits.Cpu != nil {
-										//Results returned in cores. Converting them to nanocores
-										containerMetricValue := container.Resources.Limits.Cpu().Value()
-										metricValueMultiplier := math.Pow(1000, 3)
-										metricValue = float64(containerMetricValue) * metricValueMultiplier
-									} else {
-										nodeMetricsHashKey := clusterId + "/" + nodeName + "_" + "allocatable" +  "_" + metricNameToCollect
-										if NodeMetrics != nil && NodeMetrics[nodeMetricsHashKey] != 0 {
-											metricValue = NodeMetrics[nodeMetricsHashKey]
-										}
-									}
-								} else if metricCategory == "requests" {
-									containerMetricValue := container.Resources.Requests.Cpu().Value()
-									metricValueMultiplier := math.Pow(1000, 3)
-									metricValue = float64(containerMetricValue) * metricValueMultiplier
-								}
-					case "memory": if metricCategory == "limits" {
-										metricValue = float64(container.Resources.Limits.Memory().Value())
-									} else if metricCategory == "requests" {
-										metricValue = float64(container.Resources.Requests.Memory().Value())
-									}
-				}
-				record["Timestamp"] = metricTime
-				record["Host"] = nodeName
-				record["ObjectName"] = "K8SContainer"
-				record["InstanceName"] = clusterId + "/" + string(podUid)+ "/" + containerName
-				metricCounter := MetricCollection {
-					CounterName: metricNametoReturn,
-					Value: metricValue,
-				}
-				metricCounterItems = append(metricCounterItems, metricCounter)
-				record["Collections"] = metricCounterItems
-
-				mapstructure.Decode(record, &metricItem)
-				metricItems = append(metricItems, metricItem)
-			}
-		}
-	}
-	return metricItems
-}
-
-func parseNodeLimits(nodes *v1.NodeList, metricCategory string, metricNameToCollect string, metricNametoReturn string) []DataItem {
-	var metricItems []DataItem
-	clusterId := getClusterId()
-	currentTime := time.Now()
-	metricTime := currentTime.UTC().Format(time.RFC3339)
-	for _, node := range nodes.Items {
-		var metricItem DataItem
-		var metricCounterItems []MetricCollection
-
-		nodeMetaData := node.ObjectMeta
-		nodeName := nodeMetaData.Name
-		var metricValue float64
-		//if node.Status != nil {
-			//metricValue := getMetricNumericValue(metricNameToCollect, node.Status.MetricCategory.MetricNameToCollect)
-			//metricValue := getMetricNumericValue(metricNameToCollect, node.Status.MetricCategory.MetricNameToCollect)
-			switch metricNameToCollect {
-				case "cpu": if metricCategory == "allocatable" {
-								nodeMetricValue := node.Status.Allocatable.Cpu().Value()
-								metricValueMultiplier := math.Pow(1000, 3)
-								metricValue = float64(nodeMetricValue) * metricValueMultiplier
-							} else if metricCategory == "capacity" {
-								nodeMetricValue := node.Status.Capacity.Cpu().Value()
-								metricValueMultiplier := math.Pow(1000, 3)
-								metricValue = float64(nodeMetricValue) * metricValueMultiplier
-							}
-				case "memory": if metricCategory == "allocatable" {
-									metricValue = float64(node.Status.Allocatable.Memory().Value())
-								} else if metricCategory == "capacity" {
-									metricValue = float64(node.Status.Capacity.Memory().Value())
-								}
-			}
-			record := make(map[interface{}]interface{})
-			record["Timestamp"] = metricTime
-			record["Host"] = nodeName + "rashmi"
-			record["ObjectName"] = "K8SNode"
-			record["InstanceName"] = clusterId + "/" + nodeName + "/rashmi"
-			metricCounter := MetricCollection {
-				CounterName: metricNametoReturn,
-				Value: metricValue,
-			}
-			metricCounterItems = append(metricCounterItems, metricCounter)
-			record["Collections"] = metricCounterItems
-
-			mapstructure.Decode(record, &metricItem)
-			metricItems = append(metricItems, metricItem)
-			//push node level metrics to a inmem hash so that we can use it looking up at container level.
-			//Currently if container level cpu & memory limits are not defined we default to node level limits
-			NodeMetrics := make(map[string]float64)
-			NodeMetrics[clusterId + "/" + nodeName + "_" + metricCategory + "_" + metricNameToCollect] = metricValue
-			//@Log.info ("Node metric hash: #{@@NodeMetrics}")
-		//}
-	}
-	return metricItems
-}
-
-
 func enumerate() {
 	config, err := rest.InClusterConfig()
 	if err != nil {
@@ -261,18 +127,6 @@ func enumerate() {
 	if err != nil {
 		panic(err.Error())
 	}
-	fmt.Println("Getting pods...")
-	pods, err := clientset.CoreV1().Pods("").List(metav1.ListOptions{})
-	if err != nil {
-		panic(err.Error())
-	}
-	fmt.Println("Done getting pods...")
-	var dataItems []DataItem
-	dataItems = append(dataItems, getContainerResourceRequestsAndLimits(pods, "requests", "cpu","cpuRequestNanoCores")...)
-	dataItems = append(dataItems, getContainerResourceRequestsAndLimits(pods, "requests", "memory","memoryRequestBytes")...)
-	dataItems = append(dataItems, getContainerResourceRequestsAndLimits(pods, "limits", "cpu","cpuLimitNanoCores")...)
-	dataItems = append(dataItems, getContainerResourceRequestsAndLimits(pods, "limits", "memory","memoryLimitBytes")...)
-
 	fmt.Println("Getting nodes...")
 
 	nodes, err := clientset.CoreV1().Nodes().List(metav1.ListOptions{})
@@ -280,19 +134,67 @@ func enumerate() {
 		panic(err.Error())
 	}
 	fmt.Println("Done getting nodes...")
-	dataItems = append(dataItems, parseNodeLimits(nodes, "allocatable", "cpu", "cpuAllocatableNanoCores")...)
-	dataItems = append(dataItems, parseNodeLimits(nodes, "allocatable", "memory", "memoryAllocatableBytes")...)
-	dataItems = append(dataItems, parseNodeLimits(nodes, "capacity", "cpu", "cpuCapacityNanoCores")...)
-	dataItems = append(dataItems, parseNodeLimits(nodes, "capacity", "memory", "memoryCapacityBytes")...)
+	var nodeItems []DataItem
+	//clusterId := getClusterId()
+	currentTime := time.Now()
+	fmt.Println(reflect.TypeOf(currentTime))
+	batchTime := currentTime.UTC().Format(time.RFC3339)
+	for _, node := range nodes.Items {
+		var nodeItem DataItem
+		nodeMetaData := node.ObjectMeta
+		record := make(map[interface{}]interface{})
+		//This is the time that is mapped to become TimeGenerated
+		record["CollectionTime"] = batchTime
+		record["Computer"] = nodeMetaData.Name
+		record["ClusterName"] = getClusterName()
+		record["ClusterId"] = getClusterId()
+		record["CreationTimeStamp"] = nodeMetaData.CreationTimestamp.Format(time.RFC3339)
+		nodeLabelsField, err := json.Marshal(nodeMetaData.Labels)
+		if (err == nil) {
+			nodeLabelsString := "[" + string(nodeLabelsField) + "]"
+			record["Labels"] = nodeLabelsString
+		}
 
-	podEntry := KubePerfBlob{
-		DataType:  "LINUX_PERF_BLOB",
-		IPName:    "LogManagement",
-		DataItems: dataItems,
+		record["Status"] = ""
+		//Refer to https://kubernetes.io/docs/concepts/architecture/nodes/#condition for possible node conditions.
+        //We check the status of each condition e.g. {"type": "OutOfDisk","status": "False"} . Based on this we 
+        //populate the KubeNodeInventory Status field. A possible value for this field could be "Ready OutofDisk"
+        //implying that the node is ready for hosting pods, however its out of disk.
+		if len(node.Status.Conditions) > 0 {
+			allNodeConditions := ""
+			for _, condition := range node.Status.Conditions {
+				if condition.Status == "True" {
+					if (len(allNodeConditions) > 0) {
+						allNodeConditions = allNodeConditions + "," + string(condition.Type)
+					} else {
+						allNodeConditions = string(condition.Type)
+					}
+				}
+				//collect last transition to/from ready (no matter ready is true/false)
+				if condition.Type == "Ready" && condition.LastTransitionTime != (metav1.Time{}) {
+					record["LastTransitionTimeReady"] = condition.LastTransitionTime.Format(time.RFC3339)
+				}
+			}
+			if len(allNodeConditions) > 0 {
+				record["Status"] = allNodeConditions
+			}
+		}
+		
+		record["KubeletVersion"] = node.Status.NodeInfo.KubeletVersion
+		record["KubeProxyVersion"] = node.Status.NodeInfo.KubeProxyVersion
+			
+		mapstructure.Decode(record, &nodeItem)
+		nodeItems = append(nodeItems, nodeItem)
+			
+	}
+	nodeEntry := KubeNodeInventoryBlob{
+		DataType:  "KUBE_NODE_INVENTORY_BLOB",
+		IPName:    "ContainerInsights",
+		DataItems: nodeItems,
 	}
 
-	marshalled, err := json.Marshal(podEntry)
-        //fmt.Println(podEntry)
+	marshalled, err := json.Marshal(nodeEntry)
+	
 	cert, err := tls.LoadX509KeyPair(*certFile, *keyFile)
 	if err != nil {
 		log.Fatal(err)
@@ -320,9 +222,9 @@ func enumerate() {
 
 //export FLBPluginFlush
 func FLBPluginFlush(data unsafe.Pointer, length C.int, tag *C.char) int {
-	fmt.Println("kubeperf-Starting the application...")
+	fmt.Println("Kubenodes-Starting the application...")
 	enumerate()
-	fmt.Println("kubeperf-Terminating")
+	fmt.Println("Kubenodes-Terminating")
 
 	return output.FLB_OK
 }
@@ -334,5 +236,4 @@ func FLBPluginExit() int {
 
 func main() {
 }
-
 
